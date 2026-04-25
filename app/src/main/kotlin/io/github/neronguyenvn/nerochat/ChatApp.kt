ee3d6b60491cd43e5eb42c06a3792ebec8160c2e
neronguyenvn <neronguyenvn@gmail.com>
2026-04-25 23:04:47 +0700

    feat: rename application class and update security settings
    - Rename `NeroChatApp` to `ChatApp` and update the main entry point.
    - Update `SecurityConfig` to require authentication only for `/api/**` endpoints and permit all others.
    - Remove redundant `authenticationEntryPoint` configuration.

--- app/src/main/kotlin/io/github/neronguyenvn/nerochat/ChatApp.kt
+++ app/src/main/kotlin/io/github/neronguyenvn/nerochat/ChatApp.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class NeroChatApp
+class ChatApp
 
 fun main(args: Array<String>) {
-	runApplication<NeroChatApp>(*args)
+	runApplication<ChatApp>(*args)
 }
